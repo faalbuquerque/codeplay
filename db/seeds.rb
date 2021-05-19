4568eb61b16a19d7497192c25e6c9ce3ff6a22c0
Fabiane Albuquerque <bii.nett@gmail.com>
2021-05-19 01:39:42 -0300

    Update style and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,10 @@
 course_ruby = Course.create!(name: 'Ruby', description: 'Um curso de Ruby',
-                             code: 'RUBY-BASIC', price: 800,
+                             code: 'RUBY-BASIC', price: 700,
                              enrollment_deadline: '22/12/2033')
+
+course_css = Course.create!(name: 'Rails', description: 'Um curso de Rails',
+                            code: 'Rails-BASIC', price: 650,
+                            enrollment_deadline: '26/03/2074')
 
 course_html = Course.create!(name: 'HTML', description: 'Um curso de HTML',
                              code: 'HTML-BASIC', price: 300,
@@ -9,3 +13,7 @@ course_html = Course.create!(name: 'HTML', description: 'Um curso de HTML',
 course_css = Course.create!(name: 'CSS', description: 'Um curso de CSS',
                             code: 'CSS-BASIC', price: 500,
                             enrollment_deadline: '25/10/2053')
+
+course_css = Course.create!(name: 'Javascript', description: 'Um curso de Javascript',
+                            code: 'JS-BASIC', price: 325,
+                            enrollment_deadline: '26/08/2054')
