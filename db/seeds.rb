014f578e2eaa28764708f9894ed861470281061b
Fabiane Albuquerque <bii.nett@gmail.com>
2021-05-30 04:05:54 -0300

    Fix seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,23 @@
-course_ruby = Course.create!(name: 'Ruby', description: 'Um curso de Ruby',
-                             code: 'RUBY-BASIC', price: 700,
-                             enrollment_deadline: '22/12/2033')
+teacher = Teacher.create!(name: 'Bob', email: 'bob@email.com',
+                          bio: 'Eu sou o professor Bob')
 
-course_css = Course.create!(name: 'Rails', description: 'Um curso de Rails',
-                            code: 'Rails-BASIC', price: 650,
-                            enrollment_deadline: '26/03/2074')
+course_ruby = teacher.courses.create!(name: 'Ruby', description: 'Um curso de Ruby',
+                                      code: 'RUBY-BASIC', price: 700,
+                                      enrollment_deadline: '22/12/2033')
 
-course_html = Course.create!(name: 'HTML', description: 'Um curso de HTML',
-                             code: 'HTML-BASIC', price: 300,
-                             enrollment_deadline: '24/11/2043')
+course_rails = teacher.courses.create!(name: 'Rails', description: 'Um curso de Rails',
+                                     code: 'Rails-BASIC', price: 650,
+                                     enrollment_deadline: '26/03/2074')
 
-course_css = Course.create!(name: 'CSS', description: 'Um curso de CSS',
-                            code: 'CSS-BASIC', price: 500,
-                            enrollment_deadline: '25/10/2053')
+lesson_rspec = course_rails.lessons.create!(name:'Rspec com Ruby on Rails',
+                                           duration: 50,
+                                           content: 'Aula de testes com Rspec')
 
-course_css = Course.create!(name: 'Javascript', description: 'Um curso de Javascript',
-                            code: 'JS-BASIC', price: 325,
-                            enrollment_deadline: '26/08/2054')
+lesson_css = course_rails.lessons.create!(name:'Css com Rails', duration: 40,
+                                        content: 'Aula de CSS com Rails')
+
+lesson_tdd = course_ruby.lessons.create!(name:'TDD em Ruby on Rails', duration: 30,
+                                        content: 'Aula de testes em RoR')
+
+user = User.create!(email:'user@email.com', password: '123456',
+                    password_confirmation: '123456')
